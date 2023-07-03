1e1623773d7f30a213f8bc6cbbd8fb116dca5b36
jahidulislamjahid <info.jahidulislamjahid@gmail.com>
2023-07-03 14:28:05 +0600

    Auto code genration & suggestion added

--- plugins/user.lua
+++ plugins/user.lua
@@ -14,6 +14,19 @@ return {
     name = "vscode",
     -- priority=1000,
   },
+  -- Remove the `use` here if you're using folke/lazy.nvim.
+  {
+    "Exafunction/codeium.vim",
+    lazy = false,
+    name = "codeium",
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set("i", "<Tab>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+      vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
+      vim.keymap.set("i", "<c-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
+      vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+    end,
+  },
   -- {
   --   "catppuccin/nvim",
   --   name = "catppuccin",
